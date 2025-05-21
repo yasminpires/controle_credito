6967d602fa1ce9536688457961f01c95267b509e
yasminpires <yasminrodrigues211@gmail.com>
2025-05-21 15:05:36 -0300

    Adiciona script de controle de crédito

---
+++ controle_credito.py
@@ -0,0 +1,8 @@
+LIMITE = 5000
+valor_compra = float(input("Digite o valor da compra: "))
+if valor_compra > LIMITE:
+    print("Compra precisa de aprovação.")
+else:
+    print("Compra aprovada automaticamente.")
+    
+
