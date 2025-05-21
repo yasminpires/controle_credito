795639b9c877cf2df7ce9484274cf7d2daa89ca5
yasminpires <yasminrodrigues211@gmail.com>
2025-05-21 15:24:22 -0300

    Primeiro commit: controle de crédito

--- controle_credito.py
+++
@@ -1,8 +0,0 @@
-LIMITE = 5000
-valor_compra = float(input("Digite o valor da compra: "))
-if valor_compra > LIMITE:
-    print("Compra precisa de aprovação.")
-else:
-    print("Compra aprovada automaticamente.")
-    
-
